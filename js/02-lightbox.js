473007f90a608898f984b116dbb71dd367011cd0
NataliaValchuk <trewax321@gmail.com>
2023-03-26 21:55:30 +0300

    refactored 02-gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,40 +8,14 @@ const createGalleryItem = ({ preview, original, description }) => `
   </li>
 `;
 
-// Створюємо галерею з галерейних елементів
 const galleryMarkup = galleryItems.map(createGalleryItem).join('');
 const gallery = document.querySelector('.gallery');
+
 gallery.insertAdjacentHTML('beforeend', galleryMarkup);
 
-// Застосовуємо SimpleLightbox до галереї
 document.addEventListener('DOMContentLoaded', function () {
-  const lightbox = new SimpleLightbox('.gallery a', {
+  new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,
   });
 });
-
-
-// import { galleryItems } from './gallery-items.js';
-
-// const galleryContainer = document.querySelector('.gallery');
-// const markup = galleryItems.reduce(
-//   (acc, { original, preview, description }) =>
-//     (acc += `<li>
-//   <a class="gallery__item" href="${original}">
-//     <img
-//       class="gallery__image"
-//       src="${preview}"
-//       alt="${description}"
-//     />
-//   </a>
-// </li>`),
-//   ''
-// );
-
-// galleryContainer.insertAdjacentHTML('beforeend', markup);
-
-// const lightbox = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-// });
